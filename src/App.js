b872179ba73dadd999ceb622252cdc4abefb7a37
marijanastasijevic <77746232+marijanastasijevic@users.noreply.github.com>
2022-03-06 16:23:17 +0100

    Add reducer (#5)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import React from 'react';
 
-import classes from './App.module.css'
+import classes from './App.module.css';
+import './actions.js';
+import { useReducer } from 'react';
+import { actions } from './actions.js';
+
+const reducer = (state, {type, payload}) => {
+    switch(type) {
+        case actions.enter_digit:
+            console.log(payload);
+            return{
+                ...state,
+                newOperand: `${state.newOperand || ""}${payload}`,
+            };
+        case actions.choose_operation:
+            return;
+        case actions.delete_all:
+            return;
+        case actions.calculate:
+            return;
+        default:
+            console.log('Unknown action');
+            return;
+    }
+
+}
 
 function App() {
+    const [{newOperand, prevOperand, operation}, dispatch]= useReducer(reducer, {});
+
     return(
         <div className={classes.calculator__grid}>
             <div className={classes.display}>
-                <div className={classes.previous__operand}>123</div>
-                <div className={classes.current__operand}>456</div>
+                <div className={classes.previous__operand}>{prevOperand}{operation}</div>
+                <div className={classes.current__operand}>{newOperand}</div>
             </div>
             <button>AC</button>
             <button>%</button>
             <button>/</button>
             <button>*</button>
             <button>7</button>
-            <button>8</button>
+            <button onClick={() => {dispatch({type: actions.enter_digit, payload: 8})}}>8</button>
             <button>9</button>
             <button>-</button>
             <button>4</button>
